65d4836254ca0c96b88c9046d1f4a1e5292a2553
Arun_Bikram_Khatri <khatriarun221@gmail.com>
2023-05-11 21:03:08 +0545

    cloud gateway

--- Product-Service/src/main/java/com/kinmel/ProductService/Controller/ProductController.java
+++ Product-Service/src/main/java/com/kinmel/ProductService/Controller/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/kin-mel")
+@RequestMapping("/kin-mel/product-service")
 public class ProductController {
 
     @Autowired
